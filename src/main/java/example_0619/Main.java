b7439832bc68aac6c4eaa510f273ea256f80a6db
본ìpive0528 < pive0528@gmail.com>
2024-06-19 11:16:31 +0900

    test

---
+++ src/main/java/example_0619/Main.java
@@ -0,0 +1,22 @@
+package example_0619;
+
+// 문제 : 아래가 실행되도록 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔();
+    }
+}
+
+class 사람 {
+    public 팔 a왼팔;
+    // 클래스안에 변수를 정의 할 수 있다.
+    int 나이;
+
+}
+class 팔 {
+
+}
